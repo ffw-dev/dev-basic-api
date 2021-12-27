e47624c02f39d616e88e86731e239f31098b52fc
ffw-dev <93714234+ffw-dev@users.noreply.github.com>
2021-12-27 13:19:27 +0100

    getSummaryFromPeriod added.

--- lib/src/api_endpoints/production_clients_apis/production_client_api_billing_summary_endpoint.dart
+++ lib/src/api_endpoints/production_clients_apis/production_client_api_billing_summary_endpoint.dart
@@ -12,12 +12,12 @@ import '../../base_response.dart';
 class ProductionClientApisBillingSummaryEndpoint {
   ProductionClientApisBillingSummaryEndpoint(BaseHttpService baseHttpService);
 
-  Future<BaseResponse<BillingTimedSummaryItem>> _getSummary(int daysOld) async {
+  Future<BaseResponse<BillingTimedSummaryItem>> _getSummary(int fromDay, int? toDay) async {
     var query = {
       'period': json.encode({
         'from':
-            DateTime.now().subtract(Duration(days: daysOld)).toIso8601String(),
-        'to': DateTime.now().toIso8601String(),
+            DateTime.now().subtract(Duration(days: fromDay)).toIso8601String(),
+        'to': DateTime.now().subtract(Duration(days: toDay ?? 0)).toIso8601String(),
       }),
       'user': '',
       'collectionId': '',
@@ -50,12 +50,41 @@ class ProductionClientApisBillingSummaryEndpoint {
 
       getIt<BaseHttpService>().session = response.body.results[0];
 
-      var summaryResponse = _getSummary(fromPeriod).then((value) {
+      var summaryResponse = _getSummary(fromPeriod, null).then((value) {
         return value;
       });
 
       getIt<BaseHttpService>().session =
           null; // Remove session from baseHttpClient singleton created for client api
+
+      getIt<BaseHttpService>().dio.options.baseUrl = httpClientBaseUrlTemp;
+      return summaryResponse;
+    });
+
+  }
+
+  Future<BaseResponse<BillingTimedSummaryItem>> authenticateAndGetSummaryFromRange({int fromPeriod = 20000, required String baseURL, required String authToken}) async {
+    var httpClientBaseUrlTemp = getIt<BaseHttpService>().dio.options.baseUrl;
+    getIt<BaseHttpService>().dio.options.baseUrl = baseURL;
+
+    return await getIt<BaseHttpService>()
+        .getFetch(
+        "AuthKey/Login",
+        {
+          'token': authToken
+        },
+        false)
+        .then((value) {
+      var response = BaseResponse.fromApi<Session>(value, (value) => Session.fromJson(value));
+
+      getIt<BaseHttpService>().session = response.body.results[0];
+
+      var summaryResponse = _getSummary(fromPeriod, null).then((value) {
+        return value;
+      });
+
+      getIt<BaseHttpService>().session =
+      null; // Remove session from baseHttpClient singleton created for client api
 
       getIt<BaseHttpService>().dio.options.baseUrl = httpClientBaseUrlTemp;
       return summaryResponse;
